cbc8f6e546fb5a3624324fa65249448db7f35646
Nivratti <nivrattituito@gmail.com>
2019-12-26 14:42:17 +0530

    flask recognition api -- taken neural_models folder outside the media

--- recognition_api/settings.py
+++ recognition_api/settings.py
@@ -1,15 +1,11 @@
 import os
 
-BASE_DIR = os.getcwd()
-
-MEDIA_ROOT = os.path.join(
-    BASE_DIR, "media"
-)
+BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 
 facenet_model_file = os.path.join(
-    MEDIA_ROOT, 'neural_models/facenet_keras.h5'
+    BASE_DIR, 'neural_models/facenet_keras.h5'
 )
 
 face_verification_model_file = os.path.join(
-    MEDIA_ROOT, 'neural_models/face-verification-model.h5'
+    BASE_DIR, 'neural_models/face-verification-model.h5'
 )
