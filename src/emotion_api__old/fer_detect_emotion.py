be57e83a0df81d7f7532f0ec22854d687fb45059
Nivratti <boyanenivratti@gmail.com>
2020-07-08 09:23:49 +0530

    created a copy of emotion api folder

---
+++ src/emotion_api__old/fer_detect_emotion.py
@@ -0,0 +1,14 @@
+from fer import FER
+import cv2
+
+img = cv2.imread("images/n.jpg")
+detector = FER(mtcnn=True)
+emotions = detector.detect_emotions(img)
+
+print(emotions)
+
+if emotions:
+    top_emotion, score = detector.top_emotion(img)
+
+    print(f"top_emotion : {top_emotion}")
+    print(f"score : {score}")
