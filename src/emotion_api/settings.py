7020214cbf055cb627b2182f03aaf31ed372260f
Nivratti <boyanenivratti@gmail.com>
2020-07-10 18:24:24 +0530

    added checkpoint folder creation code to avoid gdown error while downloading file

--- src/emotion_api/settings.py
+++ src/emotion_api/settings.py
@@ -3,5 +3,6 @@ import os
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 
 CKPT_DIR = os.path.join(BASE_DIR, "ckpt")
+os.makedirs(CKPT_DIR, exist_ok=True)
 
 EMOTION_MODEL = os.environ.get("EMOTION_MODEL", "mobilenet")  # inceptionresnetv2
